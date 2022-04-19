0518d1bfed124cc2b8b05dd7c4db04fee8b35fe1
Andre Marquez <andre.marqquez@gmail.com>
2022-04-19 13:13:57 -0300

    feat:  Adiciona barra de navegação

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
+import './Header.css';
 
 class Header extends Component {
   constructor() {
@@ -29,7 +31,24 @@ class Header extends Component {
     const { name, loading } = this.state;
     return (
       <header data-testid="header-component">
-        {loading ? <Loading /> : <h2 data-testid="header-user-name">{name}</h2>}
+        {loading ? (
+          <Loading />
+        ) : (
+          <h2 data-testid="header-user-name" className="nameUser">
+            {name}
+          </h2>
+        )}
+        <nav className="nav-bar">
+          <Link to="/search" data-testid="link-to-search">
+            Search
+          </Link>
+          <Link to="/favorites" data-testid="link-to-favorites">
+            Favorites
+          </Link>
+          <Link to="/profile" data-testid="link-to-profile">
+            Profile
+          </Link>
+        </nav>
       </header>
     );
   }
