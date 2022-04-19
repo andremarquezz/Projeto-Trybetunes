04b3eb87050f704dfbc9dd5f982c8c4460710d40
Andre Marquez <andre.marqquez@gmail.com>
2022-04-19 14:04:26 -0300

    feat: Adiciona componente Search -  Requisito 05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,42 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nameSearch: '',
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState(() => ({
+      [name]: value,
+    }));
+  };
+
   render() {
+    const { nameSearch } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            name="nameSearch"
+            type="text"
+            placeholder="Nome do Artista ou Banda"
+            data-testid="search-artist-input"
+            value={ nameSearch }
+            onChange={ this.onInputChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ nameSearch.length < 2 }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
