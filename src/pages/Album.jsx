16aae073187dbbc111feadf1e2b6fe5f0e439716
Andre Marquez <andre.marqquez@gmail.com>
2022-04-19 20:04:26 -0300

    fix: Adiciona import PropTypes

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from '../components/MusicCard';
