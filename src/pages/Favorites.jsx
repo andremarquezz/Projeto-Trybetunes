773f9ba98ef332e5d89c45abb738c159479a5995
Andre Marquez <andre.marqquez@gmail.com>
2022-04-22 16:19:19 -0300

    Adiciona 12, Talvez vai precisar mudar a logica

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 
 class Favorites extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      songsFavorite: [{}],
+    };
+  }
+
+  componentDidMount() {
+    this.getFavorites();
+  }
+
+  getFavorites = async () => {
+    const apiFavoriteSongs = await getFavoriteSongs();
+    this.setState(
+      {
+        songsFavorite: apiFavoriteSongs,
+      },
+      () => {
+        this.setState({
+          loading: false,
+        });
+      },
+    );
+  };
+
+  removeSong = ({ target }) => {
+    console.log('entrei');
+    console.log(target);
+    const { songsFavorite } = this.state;
+    const newListFavorit = songsFavorite.filter((song) => song.trackName !== trackName);
+    this.setState({
+      songsFavorite: newListFavorit,
+    });
+  };
+
   render() {
+    const { loading, songsFavorite } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
+        {loading ? (
+          <Loading />
+        ) : (
+          <MusicCard songs={ songsFavorite } />
+        )}
       </div>
     );
   }
